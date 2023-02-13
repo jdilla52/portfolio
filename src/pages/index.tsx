1ad2b42b5216711f2f42280cc6594663390a6114
jacques Perrault <jperrault52@gmail.com>
2023-02-13 10:43:59 -0500

    added main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,46 +1,38 @@
-import { type NextPage } from "next";
+import {type NextPage} from "next";
 import Head from "next/head";
 import Link from "next/link";
-import ExperienceCard from "../components/experience_card";
+import Logo from "../assets/logo";
+import React from "react";
+import Linkedin from "../assets/linkedin";
+import Github from "../assets/github";
 
 const Home: NextPage = () => {
     return (
         <>
             <main className="flex min-h-screen flex-col items-center justify-center bg-stone-400
             font-sans font-normal leading-relaxed text-stone-800 font-base">
-                <div className="flex flex-col items-center justify-center gap-12 px-4 py-16 "> hello
-                    <Link href="/experience">this page!</Link>
+                <div className="w-48 p-6">
+                    <Logo/>
                 </div>
 
-                {/*<div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">*/}
-                {/*    <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">*/}
-                {/*        Create <span className="text-[hsl(280,100%,70%)]">T3</span> App*/}
-                {/*    </h1>*/}
-                {/*    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">*/}
-                {/*        <Link*/}
-                {/*            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"*/}
-                {/*            href="https://create.t3.gg/en/usage/first-steps"*/}
-                {/*            target="_blank"*/}
-                {/*        >*/}
-                {/*            <h3 className="text-2xl font-bold">First Steps →</h3>*/}
-                {/*            <div className="text-lg">*/}
-                {/*                Just the basics - Everything you need to know to set up your*/}
-                {/*                database and authentication.*/}
-                {/*            </div>*/}
-                {/*        </Link>*/}
-                {/*        <Link*/}
-                {/*            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"*/}
-                {/*            href="https://create.t3.gg/en/introduction"*/}
-                {/*            target="_blank"*/}
-                {/*        >*/}
-                {/*            <h3 className="text-2xl font-bold">Documentation →</h3>*/}
-                {/*            <div className="text-lg">*/}
-                {/*                Learn more about Create T3 App, the libraries it uses, and how*/}
-                {/*                to deploy it.*/}
-                {/*            </div>*/}
-                {/*        </Link>*/}
-                {/*    </div>*/}
-                {/*</div>*/}
+                <div className="flex flex-col items-center justify-center px-4 gap-4">
+                    <div className="flex flex-row items-center justify-center gap-10">
+                        <a className="h-10" href="https://github.com/jdilla52">
+                            <Github/>
+                        </a>
+                        <a className="h-10" href="https://linkedin.com/in/jacques-perrault">
+                            <Linkedin/>
+                        </a>
+                    </div>
+                    <ol className="flex flex-col justify-left gap-2 list-disc">
+                        <li>
+                            <Link href="/experience">experience</Link>
+                        </li>
+                        <li>
+                            <Link href="/experience">experiments</Link>
+                        </li>
+                    </ol>
+                </div>
             </main>
         </>
     );
