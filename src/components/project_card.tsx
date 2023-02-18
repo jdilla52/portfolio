ef0064fdb6940d028adc9905630f4fd0f2c80cec
jacques Perrault <jperrault52@gmail.com>
2023-02-17 21:16:03 -0500

    cleanse project content

--- src/components/project_card.tsx
+++ src/components/project_card.tsx
@@ -3,6 +3,7 @@ import React, {useEffect, useState} from "react";
 import Github from "../assets/github";
 import {Project, Section} from "../pages/projects";
 import {Role} from "../pages/experience";
+import {goTo} from "../utils/propogation";
 
 type ProjectCardProps = {
     props: Project,
@@ -16,12 +17,15 @@ const ProjectCard = ({props, expanded, setExpanded}: ProjectCardProps) => {
         if (expanded && ref.current)
             ref.current.scrollIntoView({behavior: "smooth", block: "start"})
     }, [expanded])
+
+    // todo refactor for different card states: image, text, link, image + text + link
+    // hacked in for now
     return (
-        <div className="w-full border-2 border-stone-800 font-cairo w-2/3"
+        <div className="w-full border-2 border-stone-800 font-cairo w-4/5"
              onClick={() => setExpanded()} ref={ref}>
-            <div className="w-full flex flex-row justify-between content-between pl-6 p-4 gap-7">
+            <div className="flex flex-row justify-between content-between pl-6 p-4 gap-7">
                 <div className="grow flex flex-col">
-                    <div className="flex flex-row items-center justify-between gap-3 pb-1">
+                    <div className="flex flex-row items-center justify-between gap-3 pb-2">
                         {props.thumb ? <img className="h-28 w-36" src={props.thumb}/> : null}
                         <div className="grow flex-none flex flex-col justify-center">
                             <h3 className="text-lg">{props.name}</h3>
@@ -34,28 +38,31 @@ const ProjectCard = ({props, expanded, setExpanded}: ProjectCardProps) => {
                                 return (
                                     <div key={section.details} className="flex flex-row w-full">
                                         {section.link ?
-                                            <a className="h-6 w-6 mr-4 self-end self-center justify-self-center flex-none"
-                                               href={section.link}>
+                                            <button
+                                                className="h-6 w-6 mr-4 self-end self-center justify-self-center flex-none"
+                                                onClick={(e) => goTo(e, section.link)}>
                                                 <Github/>
-                                            </a> : null}
+                                            </button> : null}
                                         {section.image ? <img className="w-1/2 flex-none" src={section.image}/> : null}
                                         <div className="flex flex-col flex-grow p-2">
                                             {section.title ?
-                                                <h4 className="flex-none">{section.title.toUpperCase()}</h4> : null}
-                                            <p className="">{section.details}</p>
-
+                                                <>
+                                                    <h4 className="flex-none">{section.title.toUpperCase()}</h4>
+                                                    <p className="">{section.details}</p>
+                                                </> : <p className="p-8">{section.details}</p>}
                                         </div>
                                     </div>
                                 )
                             })}
                         </div> : null}
                 </div>
-
                 <div
                     className="flex flex-col min-h-max justify-self-end justify-between justify-end contend-end content-between flex-none">
-                    {props.link ? <a className="h-7" href={props.link}>
-                        <Github/>
-                    </a> : null}
+                    {props.link ?
+                        <button className="h-7"
+                                onClick={(e) => goTo(e, props.link)}>
+                            <Github/>
+                        </button> : null}
                     {props.sections ? <div className="h-3 self-end justify-self-end">
                         <Arrow t={expanded ? 1.0 : 0.0}/>
                     </div> : null}
