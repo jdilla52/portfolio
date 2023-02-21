82bf728ed98447a95497eea28484879e1c9a3bf7
jacques Perrault <jperrault52@gmail.com>
2023-02-20 21:53:20 -0500

    cooler icon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,17 +10,17 @@ const Home: NextPage = () => {
     return (
         <main className="fixed w-screen h-screen flex flex-col items-center justify-center
             font-sans font-normal leading-relaxed text-stone-800 bg-stone-100 body-font font-cairo gap-5">
-            <div className="w-36">
+            <div className="w-40">
                 <Logo/>
             </div>
-            <div className="flex w-40 justify-between">
-                <a className="h-10" href="https://github.com/jdilla52">
+            <div className="flex flex-row w-40 justify-between">
+                <a className="h-10 w-10" href="https://github.com/jdilla52">
                     <Github/>
                 </a>
-                <a className="w-10 h-10" href="/resume_2023_2.pdf" download="/resume_2023_2.pdf">
+                <a className="h-10 w-10" href="/resume_2023_2.pdf" download="/resume_2023_2.pdf">
                     <Resume/>
                 </a>
-                <a className="h-10" href="https://linkedin.com/in/jacques-perrault">
+                <a className="h-10 w-10" href="https://linkedin.com/in/jacques-perrault">
                     <Linkedin/>
                 </a>
             </div>
